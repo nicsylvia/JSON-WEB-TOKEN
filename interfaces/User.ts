4691370de80bb21ed693a56d33c8e01264fa17e1
nicsylvia <nicsylvia15f@gmail.com>
2023-02-16 14:15:34 -0800

    saved user.ts

--- interfaces/User.ts
+++ interfaces/User.ts
@@ -1,17 +1,22 @@
 import { Document, Schema } from "mongoose";
 import { Request } from "express";
 
+export interface ICartItem{
+  productID: string;
+  quantity: number;
+}
+
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   confirmPassword: string;
-  // cart?: {
-  //   items: {
-  //     productId: Schema.Types.ObjectId;
-  //     quantity: number;
-  //   };
-  // }[];
+  cart?: {
+    items: {
+      productId: Schema.Types.ObjectId;
+      quantity: number;
+    };
+  }[];
   role: string;
 }
 
